a865b7668e9a3afa07acb9c4b7bb4ba3cbd6ae3d
Asif-GD <asif.coach@gmail.com>
2024-02-14 15:16:20 +0530

    mini-project 001
    - edit credits

--- 001_random_password_generator/random_password_generator.py
+++ 001_random_password_generator/random_password_generator.py
@@ -1,4 +1,4 @@
-# credit - 100 Days of Code: The Complete Python Pro Bootcamp
+# credit - Day 5 - 100 Days of Code: The Complete Python Pro Bootcamp
 # https://www.udemy.com/course/100-days-of-code/
 
 """
